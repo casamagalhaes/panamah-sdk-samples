e279ab95aa93400b244ab1567f03d051d2836496
sombriks <sombriks@gmail.com>
2019-09-06 16:39:50 -0300

    exemplo de registro inicial de assinante

--- panamah-sample-gradle/src/main/java/panamah/sample/gradle/SampleApp.java
+++ panamah-sample-gradle/src/main/java/panamah/sample/gradle/SampleApp.java
@@ -1,6 +1,7 @@
 
 package panamah.sample.gradle;
 
+import br.com.casamagalhaes.panamah.sdk.PanamahAdmin;
 import br.com.casamagalhaes.panamah.sdk.PanamahConfig;
 import br.com.casamagalhaes.panamah.sdk.PanamahStream;
 import br.com.casamagalhaes.panamah.sdk.model.PanamahFuncionario;
@@ -8,6 +9,15 @@ import br.com.casamagalhaes.panamah.sdk.model.PanamahFuncionario;
 public class SampleApp {
     public static void main(String[] args) throws Exception {
         PanamahConfig cfg = PanamahConfig.fromProperties("application");
+
+        cfg.getAuth().getAssinante().setNome("<Nome aqui>");
+        cfg.getAuth().getAssinante().setFantasia("<Nome fantasia aqui>");
+                
+        PanamahAdmin admin = PanamahAdmin.getInstance();
+
+        admin.createAssinante(cfg);
+//        admin.createAssinante(cfg);
+
         PanamahStream stream = PanamahStream.init(cfg);
         
         PanamahFuncionario model = new PanamahFuncionario("123");
