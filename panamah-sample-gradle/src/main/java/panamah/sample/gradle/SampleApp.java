cd1b5a4abe9159c1de734c5dbf930d13c2234d69
Leonardo Silveira <sombriks@gmail.com>
2019-09-06 13:21:17 -0300

    fix typo

--- panamah-sample-gradle/src/main/java/panamah/sample/gradle/SampleApp.java
+++ panamah-sample-gradle/src/main/java/panamah/sample/gradle/SampleApp.java
@@ -6,7 +6,7 @@ import br.com.casamagalhaes.panamah.sdk.PanamahConfig;
 public class SampleApp {
     public static void main(String[] args) throws Exception {
         PanamahConfig cfg = PanamahConfig.autoConfigure();
-        System.out.println(cfg);
+        System.out.println(cfg); 
 
     }
 }
