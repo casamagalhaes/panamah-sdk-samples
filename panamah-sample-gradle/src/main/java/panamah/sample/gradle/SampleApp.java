d65804c7714ff5d68489d152d6b98849b6d75272
Leonardo Silveira <sombriks@gmail.com>
2019-09-06 14:53:09 -0300

    testes com o envio de um model

--- panamah-sample-gradle/src/main/java/panamah/sample/gradle/SampleApp.java
+++ panamah-sample-gradle/src/main/java/panamah/sample/gradle/SampleApp.java
@@ -2,11 +2,19 @@
 package panamah.sample.gradle;
 
 import br.com.casamagalhaes.panamah.sdk.PanamahConfig;
+import br.com.casamagalhaes.panamah.sdk.PanamahStream;
+import br.com.casamagalhaes.panamah.sdk.model.PanamahFuncionario;
 
 public class SampleApp {
     public static void main(String[] args) throws Exception {
         PanamahConfig cfg = PanamahConfig.fromProperties("application");
-        System.out.println(cfg.getAuth().getAssinante().getId()); 
+        PanamahStream stream = PanamahStream.init(cfg);
+        
+        PanamahFuncionario model = new PanamahFuncionario("123");
+        
+        stream.delete(model);
+        
+        stream.flush(true);
 
     }
 }
