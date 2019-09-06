d1b72476eefdc6dba2075f9922f794fe73396f97
Leonardo Silveira <sombriks@gmail.com>
2019-09-06 14:45:51 -0300

    estudando o problema de carregamento dos dados

--- panamah-sample-gradle/src/main/java/panamah/sample/gradle/SampleApp.java
+++ panamah-sample-gradle/src/main/java/panamah/sample/gradle/SampleApp.java
@@ -5,8 +5,8 @@ import br.com.casamagalhaes.panamah.sdk.PanamahConfig;
 
 public class SampleApp {
     public static void main(String[] args) throws Exception {
-        PanamahConfig cfg = PanamahConfig.autoConfigure();
-        System.out.println(cfg); 
+        PanamahConfig cfg = PanamahConfig.fromProperties("application");
+        System.out.println(cfg.getAuth().getAssinante().getId()); 
 
     }
 }
